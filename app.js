9860b074a3f2096ab8613e3438fc2f375ae1f696
Omid Rajabi <omid.345@gmail.com>
2020-06-22 15:36:47 -0400

    moved react components to /client

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
